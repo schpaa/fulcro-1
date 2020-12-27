98607ce14860ef820c4a5c13b48106fd49dddf3b
schpa <chris.patrick.schreiner@gmail.com>
2020-12-28 00:30:28 +0100

    stage 2

--- src/main/app/ui.cljs
+++ src/main/app/ui.cljs
@@ -3,6 +3,8 @@
             [com.fulcrologic.fulcro.components :as comp :refer [defsc]]))
 
 (defsc Person [this {:person/keys [name age]}]
+  {:initial-state (fn [{:keys [name age]}]
+                    {:person/name name :person/age age})}
   (div
    (p "Name: " name)
    (p "Age: " age)))
@@ -10,6 +12,14 @@
 (def ui-person (comp/factory Person {:keyfn :person/name}))
 
 (defsc PersonList [this {:list/keys [label people]}]
+  {:initial-state
+   (fn [{:keys [label]}]
+     {:list/label  label
+      :list/people (if (= label "Friends")
+                     [(comp/get-initial-state Person {:name "Sally" :age 321})
+                      (comp/get-initial-state Person {:name "Joey" :age 321})]
+                     [(comp/get-initial-state Person {:name "Are" :age 321})
+                      (comp/get-initial-state Person {:name "Bo" :age 321})])})}
   (div
     (h4 label)
     (ul
@@ -17,10 +27,12 @@
 
 (def ui-person-list (comp/factory PersonList))
 
-(defsc Root [this {:keys [ui/react-key] :as props}]
+(defsc Root [this {:keys [friends enemies] :as props}]
+  {:initial-state (fn [params]
+                    {:friends (comp/get-initial-state PersonList {:label "Enemies"})
+                     :enemies (comp/get-initial-state PersonList {:label "Friends"})})}
   (div
     (h4 "TODO " (str (:fulcro.inspect.core/app-uuid props)))
     (ui-person {:person/name "Peter" :person/age 123})
-    (ui-person-list {:list/label  "Friendly"
-                     :list/people [{:person/name "Sally" :person/age 321}
-                                   {:person/name "Joey" :person/age 321}]})))
+    (ui-person-list friends)
+    (ui-person-list enemies)))
